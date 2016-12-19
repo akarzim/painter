5e03b008f1099a47bc83c04fa74da67eac84f992
François Vantomme <akarzim@gmail.com>
2016-12-19 23:02:36 +0100

    Chore (middleman): update to Middleman 4.1

---
+++ Gemfile
@@ -0,0 +1,35 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
+
+ruby '2.3.1'
+
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+# Middleman Gems
+gem 'middleman',                       '>= 4.0.0'
+gem 'middleman-autoprefixer',          '~> 2.7.1'
+gem 'middleman-compass',               '>= 4.0.0'
+gem 'middleman-favicon-maker',         '~> 4.0.4'
+gem 'middleman-gh-pages',              '~> 0.3.1'
+# gem 'middleman-imageoptim',            '~> 0.2.1'
+# gem 'middleman-imageoptim', github: 'akarzim/middleman-imageoptim'
+gem 'middleman-imageoptim',            path: '~/dev/middleman-imageoptim'
+
+gem 'middleman-livereload',            '~> 3.4.6'
+gem 'middleman-minify-html',           '~> 3.4.1'
+gem 'middleman-robots',                '~> 1.2.3'
+gem 'middleman-search_engine_sitemap', '~> 1.4.0'
+# gem 'middleman-sitemap',               '~> 0.0.13'
+# gem 'middleman-webp', github: 'akarzim/middleman-webp', branch: 'feature/before_build'
+gem 'middleman-webp',                  '~> 0.4.2'
+
+gem 'bourbon',           '~> 4.2.7', require: false
+gem 'font-awesome-sass', '~> 4.7.0', require: false
+gem 'middleman-pry',     '~> 1.0.1', require: false
+gem 'neat',              '~> 1.8.0', require: false
+gem 'slim',              '~> 3.0.7', require: false
---
+++ config.rb
@@ -0,0 +1,93 @@
+require 'pry'
+require 'slim'
+# Slim::Engine.disable_option_validator!
+require 'font-awesome-sass'
+
+###
+# Page options, layouts, aliases and proxies
+###
+activate :autoprefixer, browsers: ['last 2 versions', 'ie 8', 'ie 9']
+activate :directory_indexes
+activate :i18n
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  activate :favicon_maker, icons: {
+    File.join(config.images_dir, '_favicon_base.png') => [
+      { icon: 'apple-touch-icon-114x114-precomposed.png' },
+      { icon: 'apple-touch-icon-152x152-precomposed.png' },
+      { icon: 'apple-touch-icon-72x72-precomposed.png' },
+      { icon: 'chrome-touch-icon-192x192.png' },
+      { icon: 'favicon-160x160.png' },
+      { icon: 'favicon-16x16.png' },
+      { icon: 'favicon-196x196.png' },
+      { icon: 'favicon-32x32.png' },
+      { icon: 'favicon-96x96.png' },
+      { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
+      { icon: 'ms-touch-icon-144x144-precomposed.png', size: '144x144' }
+    ]
+  }
+
+  activate :webp do |webp|
+    webp.ignore = %r{^build\/(?!.*#{config.images_dir}\/).*}
+    webp.run_before_build = true
+  end
+
+  activate :imageoptim do |opts|
+    # Compressor worker options
+    # individual optimisers can be disabled by passing false instead of a hash
+    opts.pngout = false
+    opts.svgo = false
+  end
+
+  activate :minify_html, remove_input_attributes: false
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
+  activate :asset_hash
+
+  # activate :sitemap, hostname: app.data.settings.site.url
+  set :url_root, app.data.settings.site.url
+  activate :search_engine_sitemap
+
+  activate :robots do |opts|
+    opts.rules = [{ user_agent: '*', allow: %w(/) }]
+    opts.sitemap = File.join(app.data.settings.site.url, 'sitemap.xml')
+  end
+
+  # Use this for gh-pages
+  activate :relative_assets
+  set :relative_links, true
+end
